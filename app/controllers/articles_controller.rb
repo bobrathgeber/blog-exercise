7818244e4446f3e443f01d51d3db5510d2cac706
Bob Rathgeber <bob.rathgeber@gmail.com>
2016-10-06 13:28:04 -0500

    added initial commit for blog exercise half way through.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+def index
+  @articles = Article.all
+end
+
+  def show
+      @article = Article.find(params[:id])
+    end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #@article = Article.new(params[:article]) #params[:article] contains the attributes we want
+    @article = Article.new(article_params) #this line restricts the param instead of the top line
+
+    if @article.save                            #saves the model into the database -- returns boolean
+      redirect_to @article                      #redirect the user to the show action
+                                                #QUESTION: why does it redirect to the show action?
+    else
+      render 'new' #The render method is used so that the @article object is passed back to
+                   #the new template when it is rendered. This rendering is done within the
+                   #same request as the form submission, whereas the redirect_to will tell
+                   #the browser to issue another request.
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :articles   #A resource is the term used for a collection
+                        #of similar objects, such as articles, people
+                        #or animals. You can create, read, update and destroy
+                        #items for a resource and these operations are referred
+                        #to as CRUD operations.
+
+  root 'welcome#index'  #sets fallback page to load
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
