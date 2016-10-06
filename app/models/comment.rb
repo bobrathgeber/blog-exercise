692c760e69075f08927f2152301c51ab0b112d59
Bob Rathgeber <bob.rathgeber@gmail.com>
2016-10-06 15:21:04 -0500

    add comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+#this was generated with this command line: $ bin/rails generate model Comment commenter:string body:text article:references
+class Comment < ApplicationRecord
+  belongs_to :article
+end
