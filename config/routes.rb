9c34c4b24e8b0db9eac290b2d21aed3394742076
Bob Rathgeber <bob.rathgeber@gmail.com>
2016-10-06 15:39:15 -0500

    generate controller for comments and added a route for it

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
                         #or animals. You can create, read, update and destroy
                         #items for a resource and these operations are referred
                         #to as CRUD operations.
+  resources :comments
 
   root 'welcome#index'  #sets fallback page to load
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
